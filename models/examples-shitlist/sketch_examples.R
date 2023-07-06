8fef9cf439fa19f75af27b44bda546a415a46d7d
Cao Bittencourt Ferreira <caobittencourt@gmail.com>
2023-07-06 13:51:45 -0300

    IQ estimator
    fun_iq function to estimate IQs based on attribute proxies for intelligence
    Transposed the 7-point Likert scale to an IQ scale to assist in writing difficulty level examples

--- models/examples-shitlist/sketch_examples.R
+++ models/examples-shitlist/sketch_examples.R
@@ -13,7 +13,12 @@ df_occupations %>%
       occupation
     ), 'account'
   )) %>% 
-  pull(economics_and_accounting.l)
+  select(
+    occupation
+    , economics_and_accounting.l
+    , entry_level_education
+    , annual_wage_2021
+  ) %>% view
 
 df_occupations %>% 
   select(
@@ -28,25 +33,216 @@ df_occupations %>%
 # deal_with_unpleasant_or_angry_people
 # deductive_reasoning
 # biology
-
+df_occupations %>% 
+  select(
+    employment2
+    , ends_with('.l')
+  ) %>% 
   pivot_longer(
     cols = -1
   ) %>% 
   filter(
-    value != 0
+    # value != 0
+    str_detect(
+      name,
+      # 'mathematics'
+      # 'english'
+      'english|mathematics'
+    )
   ) %>% 
   reframe(
     mean = 
       weighted.mean(
-      value, employment2
-    )
+        value, employment2
+      )
     , sd = 
       sqrt(wtd.var(
         value, employment2
       ))
   )
-  ggplot(aes(
-    x = value
-    , weight = employment2
-  )) + 
-  geom_density()
+# ggplot(aes(
+#   x = value
+#   , weight = employment2
+# )) + 
+#   geom_density()
+
+
+# read_rds(
+#   "C:/Users/Cao/Documents/Github/atlas-research/data/efa_model_equamax_15_factors.rds"
+# ) -> efa_model
+
+# df_occupations %>%
+#   select(
+#     occupation
+#     , employment2
+#     , any_of(
+#       df_factors %>%
+#         filter(
+#           str_detect(
+#             str_to_lower(
+#               factor.name
+#             ), 'intelligence'
+#           )) %>%
+#         pull(item)
+#     )
+#   ) %>% 
+#   pivot_longer(
+#     cols = ends_with('.l')
+#     , names_to = 'item'
+#     , values_to = 'item.score'
+#   ) %>% 
+#   mutate(
+#     iq.mean = 
+#       weighted.mean(
+#         item.score
+#         , employment2
+#       )
+#   ) %>% 
+#   group_by(occupation) %>% 
+#   reframe(
+#     iq = 
+#       100 * 
+#       mean(item.score) / 
+#       first(iq.mean)
+#   ) %>% 
+#   arrange(desc(iq))
+
+df_occupations %>%
+  select(
+    occupation
+    , any_of(
+      df_factors %>%
+        filter(
+          str_detect(
+            str_to_lower(
+              factor.name
+            ), 'intelligence'
+          )) %>%
+        pull(item)
+    )) %>% 
+  pivot_longer(
+    cols = -1
+    , names_to = 'item'
+    , values_to = 'item.score'
+  ) %>% 
+  group_by(occupation) %>%
+  reframe(
+    iq = 
+      fun_iq(
+        dbl_scores = item.score
+        , dbl_scale_ub = 100
+        , dbl_scale_lb = 0
+        , int_interval = 7
+        , dbl_proxy_mean = 36.1
+        , dbl_proxy_sd = 10.6
+        , dbl_iq_mean = 100
+        , dbl_iq_sd = 15
+      )
+  ) %>% 
+  # full_join(
+  #   df_occupations %>% 
+  #     select(
+  #       occupation
+  #       , employment2
+  #       )
+  # ) %>% 
+  # ggplot(aes(
+  #   x = iq
+  #   , weight = employment2
+  # )) + 
+# geom_density()
+arrange(desc(iq)) %>%
+  slice(1:100,(n()-1):n()) %>% 
+  print(n = nrow(.))
+
+df_occupations %>%
+  select(
+    occupation
+    , df_factors %>% 
+      filter(
+        str_detect(
+          str_to_lower(
+            factor.name
+          ), 'intelligence|discernment'
+        )) %>% 
+      pull(item)
+  ) %>% 
+  pivot_longer(
+    cols = -1
+    , names_to = 'item'
+    , values_to = 'item.score'
+  ) %>% 
+  group_by(occupation) %>%
+  reframe(
+    iq = 
+      fun_iq(
+        dbl_scores = item.score
+        , dbl_scale_ub = 100
+        , dbl_scale_lb = 0
+        , int_interval = 7
+        , dbl_proxy_mean = 41.1
+        , dbl_proxy_sd = 14.8
+        , dbl_iq_mean = 100
+        , dbl_iq_sd = 15
+        # , dbl_iq_sd = 10
+      )
+  ) %>% 
+  arrange(desc(iq)) %>%
+  slice(1:100,(n()-1):n()) %>% 
+  print(n = nrow(.))
+
+
+df_occupations %>% 
+  filter(
+    str_detect(
+      str_to_lower(
+        occupation
+        # ), 'econo'
+        # ), 'accountants'
+      ), 'physicists'
+      # ), 'cashier'
+    )
+  ) %>%
+  slice_sample(n = 1) -> dsds
+
+dsds$occupation
+
+dsds %>% 
+  select(
+    ends_with('.l')
+  ) %>% 
+  pivot_longer(
+    cols = everything()
+    , names_to = 'item'
+    , values_to = 'item.score'
+  ) %>% 
+  filter(
+    item %in% c(
+      df_factors %>% 
+        filter(
+          #   factor.name == 
+          #     'Intelligence'
+          # ) %>% 
+          str_detect(
+            str_to_lower(
+              factor.name
+            ), 'intelligence|discernment'
+            # ), 'intelligence'
+          )) %>% 
+        pull(item)
+    )
+  ) %>% 
+  reframe(
+    iq = fun_iq(
+      dbl_scores = item.score
+      , dbl_scale_ub = 100
+      , dbl_scale_lb = 0
+      , int_interval = 7
+      # , dbl_proxy_mean = 36.1
+      , dbl_proxy_mean = 41.1
+      # , dbl_proxy_sd = 10.6
+      , dbl_proxy_sd = 14.8
+      , dbl_iq_mean = 100
+      , dbl_iq_sd = 15
+    )
+  )
