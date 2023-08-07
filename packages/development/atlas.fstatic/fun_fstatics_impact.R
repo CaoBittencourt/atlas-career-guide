3870536db1c5716e5aa80cffaccd63c469303de2
Cao Bittencourt Ferreira <caobittencourt@gmail.com>
2023-08-07 19:04:27 -0300

    atlas.fstatic

--- packages/development/atlas.fstatic/fun_fstatics_impact.R
+++ packages/development/atlas.fstatic/fun_fstatics_impact.R
@@ -1,30 +1,30 @@
-# [SETUP] -----------------------------------------------------------------
-# - Packages ----------------------------------------------------------------
-pkg <- c(
-  # 'tidyverse' #Data wrangling
-  'dplyr', 'tidyr' #Data wrangling
-  # , 'devtools'
-  , 'atlas.ftools' #Factor analysis tools
-  # , 'stats'
-)
-
-# Activate / install packages
-lapply(pkg, function(x)
-  if(!require(x, character.only = T))
-  {install.packages(x); require(x)})
-
-install_github('CaoBittencourtFerreira/atlas.ftools')
-
-library(atlas.ftools)
-
-# Package citation
+# # [SETUP] -----------------------------------------------------------------
+# # - Packages ----------------------------------------------------------------
+# pkg <- c(
+#   # 'tidyverse' #Data wrangling
+#   'dplyr', 'tidyr' #Data wrangling
+#   # , 'devtools'
+#   , 'atlas.ftools' #Factor analysis tools
+#   # , 'stats'
+# )
+#
+# # Activate / install packages
 # lapply(pkg, function(x)
-#   {citation(package = x)})
+#   if(!require(x, character.only = T))
+#   {install.packages(x); require(x)})
+#
+# install_github('CaoBittencourtFerreira/atlas.ftools')
+#
+# library(atlas.ftools)
+#
+# # Package citation
+# # lapply(pkg, function(x)
+# #   {citation(package = x)})
 
 # [FUNCTION] ----------------------------------------------
 # - Factor-Analytic Comparative Statics ------------------------------------
-fun_coef_fstatics <- function(
-    
+fun_fstatics_impact <- function(
+
   # Data
   df_data
   # Sample weights
@@ -44,9 +44,9 @@ fun_coef_fstatics <- function(
   , dbl_immune_ub = 17
   # Aggregate results
   , lgc_aggregate = F
-  
+
 ){
-  
+
   # Arguments validation
   stopifnot(
     "'efa_model' must be a factor analysis object." =
@@ -62,12 +62,12 @@ fun_coef_fstatics <- function(
         )) == 'principal'
       )
   )
-  
+
   stopifnot(
     "'df_data' must be a data frame containing item scores." =
       is.data.frame(df_data)
   )
-  
+
   stopifnot(
     "'dbl_weights' must be a vector of sample weights the same length as 'df_data'." =
       any(
@@ -79,67 +79,67 @@ fun_coef_fstatics <- function(
             nrow()
         )
       ))
-  
+
   stopifnot(
     "'dbl_factors_impact' must be a vector of expected impact on each factor." =
       is.numeric(dbl_factors_impact)
   )
-  
+
   stopifnot(
     "'dbl_scale_lb' must be numeric." =
       is.numeric(dbl_scale_lb)
   )
-  
+
   stopifnot(
     "'dbl_scale_ub' must be numeric." =
       is.numeric(dbl_scale_ub)
   )
-  
+
   stopifnot(
     "'dbl_impact_lb' must be numeric." =
       is.numeric(dbl_impact_lb)
   )
-  
+
   stopifnot(
     "'dbl_impact_lb' must be numeric." =
       is.numeric(dbl_impact_lb)
   )
-  
+
   stopifnot(
     "'dbl_immune_lb' must be numeric." =
       is.numeric(dbl_immune_lb)
   )
-  
+
   stopifnot(
     "'dbl_immune_ub' must be numeric." =
       is.numeric(dbl_immune_ub)
   )
-  
+
   # Data wrangling
   dbl_scale_lb[[1]] -> dbl_scale_lb
   dbl_scale_ub[[1]] -> dbl_scale_ub
-  
+
   dbl_impact_lb[[1]] -> dbl_impact_lb
   dbl_impact_ub[[1]] -> dbl_impact_ub
-  
+
   dbl_immune_lb[[1]] -> dbl_immune_lb
   dbl_immune_ub[[1]] -> dbl_immune_ub
-  
+
   atlas.ftools::fun_ftools_loadings(
     efa_model
   ) -> df_loadings
-  
+
   rm(efa_model)
-  
+
   if(!any(
     names(df_data) %in%
     df_loadings$item
   )){
-    
+
     stop("'df_data' must have at least some of the columns used to estimate 'efa_model'.")
-    
+
   }
-  
+
   df_data %>%
     select(
       !where(is.numeric)
@@ -161,21 +161,21 @@ fun_coef_fstatics <- function(
         , dbl_scale_ub
       )
     ) -> df_data_long
-  
+
   rm(df_data)
-  
+
   if(
     length(dbl_factors_impact) !=
     ncol(df_loadings[-1])
   ){
-    
+
     rep(
       dbl_factors_impact[[1]]
       , ncol(df_loadings[-1])
     ) -> dbl_factors_impact
-    
+
   }
-  
+
   dbl_factors_impact %>%
     as_tibble() %>%
     set_names(
@@ -187,16 +187,16 @@ fun_coef_fstatics <- function(
         df_loadings[-1]
       )
     ) -> df_impact_factors
-  
-  df_loadings %>% 
+
+  df_loadings %>%
     pivot_longer(
       cols = !item
       , names_to = 'factor'
       , values_to = 'loading'
     ) -> df_loadings_long
-  
+
   rm(df_loadings)
-  
+
   df_loadings_long %>%
     full_join(
       df_impact_factors
@@ -219,7 +219,7 @@ fun_coef_fstatics <- function(
         sum(
           factor_impact *
             loading
-          
+
         )
       , item_impact = pmax(
         item_impact
@@ -230,7 +230,7 @@ fun_coef_fstatics <- function(
         , dbl_impact_ub
       )
     ) -> df_impact_items
-  
+
   # Calculate net impact
   df_data_long %>%
     full_join(
@@ -273,15 +273,15 @@ fun_coef_fstatics <- function(
             item_score
         )
     ) -> df_impact
-  
+
   rm(df_data_long)
-  
+
   # Aggregate results
   df_impact_agg <- NULL
   df_impact_all <- NULL
-  
+
   if(lgc_aggregate){
-    
+
     df_impact %>%
       group_by(
         across(c(
@@ -297,19 +297,19 @@ fun_coef_fstatics <- function(
         , aggregate_impact =
           aggregate_impact - 1
       ) -> df_impact_agg
-    
+
     if(!length(dbl_weights)){
-      
+
       rep(1, nrow(df_data)) -> dbl_weights
-      
+
     }
-    
+
     df_impact_agg %>%
       mutate(
         weight =
           dbl_weights
       ) -> df_impact_agg
-    
+
     df_impact_agg %>%
       reframe(
         aggregate_impact =
@@ -318,9 +318,9 @@ fun_coef_fstatics <- function(
             , weight
           )
       ) -> df_impact_all
-    
+
   }
-  
+
   # Output
   return(list(
     'factors_impact' = df_impact_factors,
@@ -336,34 +336,34 @@ fun_coef_fstatics <- function(
     'immune_lb' = dbl_immune_lb,
     'immune_ub' = dbl_immune_ub
   ))
-  
+
 }
 
 # # [TEST] ------------------------------------------------------------------
 # # - Data ------------------------------------------------------------------
 # library(readr)
 # library(tictoc)
-# 
+#
 # # read_rds(
 # read_rds(
 #   'C:/Users/Cao/Documents/Github/atlas-research/data/efa_model_equamax_15_factors.rds'
 # ) -> efa_model
-# 
+#
 # read_csv(
 #   'C:/Users/Cao/Documents/Github/Atlas-Research/Data/df_atlas_complete_equamax_15_factors.csv'
 # ) -> df_occupations
-# 
+#
 # # - Factor-analytic comparative statics -----------------------------------
 # tic()
-# fun_coef_fstatics(
+# fun_fstatics_impact(
 #   df_data =
 #     df_occupations
-#   , dbl_weights = 
+#   , dbl_weights =
 #     df_occupations$
 #     employment2
-#   , efa_model = 
+#   , efa_model =
 #     efa_model
-#   , dbl_factors_impact = 
+#   , dbl_factors_impact =
 #     runif(
 #       efa_model$
 #         factors
@@ -373,9 +373,9 @@ fun_coef_fstatics <- function(
 #   , lgc_aggregate = T
 # ) -> dsds
 # toc()
-# 
+#
 # dsds
-# 
+#
 # dsds$factors_impact %>% View
 # dsds$items_impact %>% View
 # dsds$aggregate_impact %>% View
