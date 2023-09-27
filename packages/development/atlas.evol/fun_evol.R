c46b0e4382c454104cbd4c0a2c70e86b8b948e96
Cao Bittencourt <caobittencourt@gmail.com>
2023-09-27 18:30:31 -0300

    unesco relevant items

--- packages/development/atlas.evol/fun_evol.R
+++ packages/development/atlas.evol/fun_evol.R
@@ -12,7 +12,8 @@ chr_pkg <- c(
 # Git packages
 chr_git <- c(
   'CaoBittencourt' = 'atlas.intc',
-  'CaoBittencourt' = 'atlas.match'
+  'CaoBittencourt' = 'atlas.match',
+  'CaoBittencourt' = 'atlas.comp'
 )
 
 # Activate / install CRAN packages
@@ -80,9 +81,9 @@ rm(chr_pkg, chr_git)
 # - Data ------------------------------------------------------------------
 library(readr)
 
-# read_rds(
-#   'C:/Users/Cao/Documents/Github/atlas-research-dev/data/efa/efa_equamax_14factors.rds'
-# ) -> efa_model
+read_rds(
+  'C:/Users/Cao/Documents/Github/atlas-research-dev/data/efa/efa_equamax_14factors.rds'
+) -> efa_model
 
 read_csv(
   'C:/Users/Cao/Documents/Github/Atlas-Research-dev/Data/df_occupations_2023_efa.csv'
@@ -92,50 +93,63 @@ read_csv(
 #   'https://docs.google.com/spreadsheets/d/e/2PACX-1vSVdXvQMe4DrKS0LKhY0CZRlVuCCkEMHVJHQb_U-GKF21CjcchJ5jjclGSlQGYa5Q/pub?gid=1515296378&single=true&output=csv'
 # ) -> df_input
 
-# # - Interchangeability ----------------------------------------------------
-# fun_match_similarity(
-#   df_data_rows = df_occupations,
-#   df_query_rows = df_occupations,
-#   chr_method = 'bvls',
-#   dbl_scale_ub = 100,
-#   dbl_scale_lb = 0,
-#   chr_id_col = 'occupation'
-# ) -> list_similarity
-# 
-# list_similarity$
-#   mtx_similarity
-# 
-# fun_intc_interchangeability(
-#   dbl_similarity = 
-#     list_similarity$
-#     mtx_similarity
-# ) -> mtx_interchangeability
-# 
-# list_similarity$
-#   mtx_similarity %>% 
-#   as_tibble() %>% 
-#   map_df(fun_intc_interchangeability) %>% 
-#   as.matrix() -> 
-#   # mtx_dist
-#   mtx_interchangeability
-# 
-# # 1 - mtx_dist -> mtx_dist
-# 
-# # colnames(mtx_dist) ->
-# #   rownames(mtx_dist)
-# 
-# colnames(mtx_interchangeability) ->
-#   rownames(mtx_interchangeability)
-# 
-# 
-# dist(
-#   mtx_interchangeability
-#   , method = 'euclidean'
-# ) -> dist_interchangeability
-# 
-# hclust(dist_interchangeability) -> list_hclust
-# 
-# 
-# 
-# 
-# 
+# - Interchangeability ----------------------------------------------------
+fun_match_similarity(
+  df_data_rows = df_occupations[2:10,],
+  df_query_rows = df_occupations[1,],
+  chr_method = 'bvls',
+  dbl_scale_ub = 100,
+  dbl_scale_lb = 0,
+  chr_id_col = 'occupation'
+) -> list_similarity
+
+list_similarity$
+  mtx_similarity
+
+fun_intc_interchangeability(
+  dbl_similarity =
+    list_similarity$
+    mtx_similarity
+) -> mtx_interchangeability
+
+list_similarity$
+  mtx_similarity %>%
+  as_tibble() %>%
+  map_df(fun_intc_interchangeability) %>%
+  as.matrix() ->
+  # mtx_dist
+  mtx_interchangeability
+
+# 1 - mtx_dist -> mtx_dist
+
+# colnames(mtx_dist) ->
+#   rownames(mtx_dist)
+
+colnames(mtx_interchangeability) ->
+  rownames(mtx_interchangeability)
+
+
+dist(
+  mtx_interchangeability
+  , method = 'euclidean'
+) -> dist_interchangeability
+
+hclust(dist_interchangeability) -> list_hclust
+
+
+
+
+
+
+# - Competence ------------------------------------------------------------
+df_occupations %>% 
+  transmute(
+    occupation = occupation,
+    competence = 
+      df_occupations %>% 
+      select(any_of(rownames(loadings(efa_model)[,]))) %>% 
+      apply(1, fun_comp_competence)
+  ) %>% 
+  arrange(desc(
+    competence
+  )) -> df_competence
