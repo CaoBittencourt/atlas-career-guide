38cebefd0a04a2b77af3db5e2778300e11148e69
Cao Bittencourt <caobittencourt@gmail.com>
2024-12-11 19:24:09 -0300

    unit tests

--- tests/describe.r
+++ tests/describe.r
@@ -1,3 +1,4 @@
+# setup
 # region: modules
 # install box if not installed
 if (!any(utils::installed.packages()[, 1] == "box")) {
@@ -18,16 +19,275 @@ project.options(
     atlas.src = "src",
     atlas.mod = "src/mod",
     box.path = "src",
-    atlas.data = "data"
+    atlas.data = "data",
+    atlas.occupations = "/data/occupations/df_occupations_2022.csv"
   ),
   root.name = ".atlas"
 )
 
 # endregion
-# region: run tests
-getOption("atlas.root") |>
-  file.path("tests", "describe") |>
-  file.path("tests.r") |>
-  source()
+# region: imports
+box::use(
+  desc = mod / describe,
+  util = mod / utils
+)
+
+# endregion
+# region: data
+# hyper-generalist skill set
+runif(1) |> rep(120) -> hyper_generalist
+
+# hyper-specialist skill set
+runif(1) |> c(rep(0, 119)) -> hyper_specialist
+
+# endregion
+# tests
+# region: attribute equivalence submodule
+test_aeq <- function() {
+  return(
+    list(
+      "equivalent attributes form a valid skill set" =
+        runif(120) |>
+          desc$eq$aeq() |>
+          util$assert$valid_skill_set() |>
+          try() |>
+          class() !=
+          "try-error",
+      "invalid skill sets attribute equivalence" = list(
+        "attribute > 1" =
+          c(rep(0, 119), 1.1) |>
+            desc$eq$aeq() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute < 0" =
+          c(rep(0, 119), -0.1) |>
+            desc$eq$aeq() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == Inf" =
+          c(rep(0, 119), Inf) |>
+            desc$eq$aeq() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == NA" =
+          c(rep(0, 119), NA) |>
+            desc$eq$aeq() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute not numeric" =
+          c(rep(0, 119), "dsds") |>
+            desc$eq$aeq() |>
+            try() |>
+            class() ==
+            "try-error"
+      ),
+      "unit vector attribute equivalence" =
+        all(
+          rep(1, 120) |> desc$eq$aeq() == 1,
+          rep(1, 120) |> desc$eq$aeq() |> length() == 120
+        ),
+      "null vector attribute equivalence" =
+        all(
+          rep(0, 120) |> desc$eq$aeq() == 1,
+          rep(0, 120) |> desc$eq$aeq() |> length() == 120
+        ),
+      "hyper-generalist attribute equivalence" =
+        all(
+          hyper_generalist |> desc$eq$aeq() == 1,
+          hyper_generalist |> desc$eq$aeq() |> length() == 120
+        ),
+      "hyper-specialist attribute equivalence" =
+        all(
+          hyper_specialist |> desc$eq$aeq() == c(1, rep(0, 119)),
+          hyper_specialist |> desc$eq$aeq() |> length() == 120
+        )
+    )
+  )
+}
+
+# endregion
+# region: generality submodule
+test_gene <- function() {
+  return(
+    list(
+      "invalid skill sets generality" = list(
+        "attribute > 1" =
+          c(rep(0, 119), 1.1) |>
+            desc$gn$gene() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute < 0" =
+          c(rep(0, 119), -0.1) |>
+            desc$gn$gene() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == Inf" =
+          c(rep(0, 119), Inf) |>
+            desc$gn$gene() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == NA" =
+          c(rep(0, 119), NA) |>
+            desc$gn$gene() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute not numeric" =
+          c(rep(0, 119), "dsds") |>
+            desc$gn$gene() |>
+            try() |>
+            class() ==
+            "try-error"
+      ),
+      "hyper-generalist generality" =
+        all(
+          hyper_generalist |> desc$gn$gene() == 1,
+          hyper_generalist |> desc$gn$gene() |> length() == 1
+        ),
+      "hyper-specialist generality" =
+        all(
+          hyper_specialist |> desc$gn$gene() == 0,
+          hyper_specialist |> desc$gn$gene() |> length() == 1
+        ),
+      "unit vector generality" =
+        all(
+          desc$gn$gene(rep(1, 120)) == 1,
+          desc$gn$gene(rep(1, 120)) |> length() == 1
+        ),
+      "null vector generality" =
+        all(
+          desc$gn$gene(rep(0, 120)) == 1,
+          desc$gn$gene(rep(0, 120)) |> length() == 1
+        )
+    )
+  )
+}
+
+# endregion
+# region: competence submodule
+test_comp <- function() {
+  return(
+    list(
+      "invalid skill sets competence" = list(
+        "attribute > 1" =
+          c(rep(0, 119), 1.1) |>
+            desc$cp$comp() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute < 0" =
+          c(rep(0, 119), -0.1) |>
+            desc$cp$comp() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == Inf" =
+          c(rep(0, 119), Inf) |>
+            desc$cp$comp() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute == NA" =
+          c(rep(0, 119), NA) |>
+            desc$cp$comp() |>
+            try() |>
+            class() ==
+            "try-error",
+        "attribute not numeric" =
+          c(rep(0, 119), "dsds") |>
+            desc$cp$comp() |>
+            try() |>
+            class() ==
+            "try-error"
+      ),
+      "hyper-generalist competence" =
+        all(
+          hyper_generalist |> desc$cp$comp() == hyper_generalist[[1]],
+          hyper_generalist |> desc$cp$comp() |> length() == 1
+        ),
+      "hyper-specialist competence" =
+        all(
+          hyper_specialist |> desc$cp$comp() == max(hyper_specialist),
+          hyper_specialist |> desc$cp$comp() |> length() == 1
+        ),
+      "unit vector competence" =
+        all(
+          desc$cp$comp(rep(1, 120)) == 1,
+          desc$cp$comp(rep(1, 120)) |> length() == 1
+        ),
+      "null vector competence" =
+        all(
+          desc$cp$comp(rep(0, 120)) == 0,
+          desc$cp$comp(rep(0, 120)) |> length() == 1
+        )
+    )
+  )
+}
+# whereas generality of repeated skill set is 1, competence is not
+runif(1) |> rep(120) -> skills
+all(
+  desc$cp$comp(skills) != 1,
+  desc$cp$comp(skills) |> length() == 1
+)
+rm(skills)
+
+# 1 competence
+all(
+  desc$cp$comp(rep(1, 120)) == 1,
+  desc$cp$comp(rep(1, 120)) |> length() == 1
+)
+
+# 0 competence
+all(
+  desc$cp$comp(c(rep(0, 120))) == 0,
+  desc$cp$comp(rep(1, 120)) |> length() == 1
+)
+
+# invalid skill sets competence
+c(rep(0, 119), 1.1) |>
+  desc$cp$comp() |>
+  try() |>
+  class() ==
+  "try-error"
+
+c(rep(0, 119), -0.1) |>
+  desc$cp$comp() |>
+  try() |>
+  class() ==
+  "try-error"
+
+c(rep(0, 119), Inf) |>
+  desc$cp$comp() |>
+  try() |>
+  class() ==
+  "try-error"
+
+c(rep(0, 119), "dsds") |>
+  desc$cp$comp() |>
+  try() |>
+  class() ==
+  "try-error"
+
+# endregion
+# region: run all tests
+list(
+  aeq = test_aeq(),
+  gene = test_gene(),
+  comp = test_comp()
+) |>
+  lapply(
+    function(submodule) {
+      submodule |>
+        unlist() |>
+        all()
+    }
+  )
 
 # endregion
