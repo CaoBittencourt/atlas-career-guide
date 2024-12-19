303738997982928df379ea472d10eb46eadf2c17
Cao Bittencourt <caobittencourt@gmail.com>
2024-12-19 20:32:47 -0300

    foreign key constraints

--- database/output.r
+++ database/output.r
@@ -1,13 +1,27 @@
 # imports
-box::use(tidyr[pivot_wider])
+box::use(tidyr[pivot_wider, as_tibble])
 
 # pivot skill set matrix
-read.csv("./output/skill_sets.csv") |>
+read.csv("output/skill_sets.csv") |>
   pivot_wider(
     names_from = "item",
     values_from = "item_score"
-  ) |>
-  write.csv(
-    "./output/skill_sets.csv",
-    row.names = F
-  )
+  ) -> skill_sets
+
+# csv
+skill_sets |> write.csv("output/skill_sets.csv", row.names = F)
+
+# rds
+dir.create("output/rds/", recursive = T)
+
+skill_sets |>
+  as_tibble() |>
+  saveRDS("output/rds/skill_sets.rds")
+
+read.csv("output/education.csv") |>
+  as_tibble() |>
+  saveRDS("output/rds/education.rds")
+
+read.csv("output/labor.csv") |>
+  as_tibble() |>
+  saveRDS("output/rds/labor.rds")
---
+++ database/test.r
@@ -0,0 +1,74 @@
+# modules
+modular::project.options("atlas")
+
+# imports
+library(dplyr)
+library(tidyr)
+
+# working directory
+getOption("atlas.db") |> setwd()
+
+# data
+readRDS('output/rds/skill_sets.rds') -> skill_set_mtx
+readRDS('output/rds/education.rds') -> education
+readRDS('output/rds/labor.rds') -> labor
+
+# check if data matches
+skill_set_mtx |>
+  inner_join(education) |>
+  inner_join(labor) |>
+  relocate(
+    occupation,
+    education,
+    education_years,
+    starts_with("employment"),
+    wage
+  ) |>
+  as_tibble() ->
+occupations
+
+read.csv("data/old/occupations/df_occupations_2022.csv") |> as_tibble() -> df_occupations
+
+occupations |>
+  select(
+    occupation,
+    wage,
+    employment_variants,
+    education,
+    education_years
+  ) |>
+  inner_join(
+    df_occupations |>
+      select(
+        occupation,
+        df.wage = wage,
+        df.employment_variants = employment_variants,
+        df.education = education,
+        df.education_years = education_years
+      )
+  ) -> dsds
+
+dsds |>
+  reframe(
+    wage = all(df.wage == wage),
+    employment_variants = all(df.employment_variants == employment_variants),
+    education = all(df.education == education),
+    education_years = all(df.education_years == education_years)
+  )
+
+dsds |>
+  filter(
+    df.employment_variants != employment_variants,
+  ) |>
+  mutate(
+    diff = df.employment_variants - employment_variants
+  ) |>
+  pull(diff) |>
+  summary()
+
+dsds |>
+  mutate(
+    diff = df.employment_variants - employment_variants
+  ) |>
+  pull(diff) |>
+  summary()
