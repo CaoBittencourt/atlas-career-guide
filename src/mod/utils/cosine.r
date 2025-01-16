9f30ba38a8377ff6e6e6c35ec7f4921f9af087d9
Cao Bittencourt <caobittencourt@gmail.com>
2025-01-16 07:53:48 -0300

    alternatives to cosine similarity

--- src/mod/utils/cosine.r
+++ src/mod/utils/cosine.r
@@ -1,4 +1,3 @@
 cosine.similarity <- function(ak, aq) {
-  # return(sum(ak * aq) / sqrt(sum(ak^2) * sum(aq^2)))
-  return(sum(ak * aq) / sqrt((sum(ak^2)) * (sum(aq^2))))
+  return(sum(ak * aq) / sqrt(sum(ak^2) * sum(aq^2)))
 }
