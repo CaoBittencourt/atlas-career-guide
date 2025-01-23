ab6fe6ff3990ff6399301dd5f502dd0e5e904ee0
Cao Bittencourt <caobittencourt@gmail.com>
2025-01-23 18:28:20 -0300

    vectorized qa working

--- models/qa/qa.r
+++ models/qa/qa.r
@@ -20,31 +20,31 @@ df_occupations[[2]] -> skill_set
 # model
 # region: unweighted overqualification model
 skill_set |> qa$oqa(df_occupations[-1])
-# skill_set |> qa$oqa(as.matrix(df_occupations[-1]))
+df_occupations[1:3] |> qa$oqa(df_occupations[1:19])
 
 # endregion
 # region: equivalent overqualification model
 skill_set |> qa$oqa(df_occupations[-1], aeq_method = "linear-logistic")
-# skill_set |> qa$oqa(as.matrix(df_occupations[-1]), aeq_method = "linear-logistic")
+df_occupations[1:3] |> qa$oqa(df_occupations[1:19], aeq_method = "linear-logistic")
 
 # endregion
 # region: unweighted underqualification model
 skill_set |> qa$uqa(df_occupations[-1])
-# skill_set |> qa$uqa(as.matrix(df_occupations[-1]))
+df_occupations[1:3] |> qa$uqa(df_occupations[1:19])
 
 # endregion
 # region: equivalent underqualification model
 skill_set |> qa$uqa(df_occupations[-1], aeq_method = "linear-logistic")
-# skill_set |> qa$uqa(as.matrix(df_occupations[-1]), aeq_method = "linear-logistic")
+df_occupations[1:3] |> qa$uqa(df_occupations[1:19], aeq_method = "linear-logistic")
 
 # endregion
 # region: unweighted sufficient qualification model
 skill_set |> qa$sqa(df_occupations[-1])
-# skill_set |> qa$sqa(as.matrix(df_occupations[-1]))
+df_occupations[1:3] |> qa$sqa(df_occupations[1:19])
 
 # endregion
 # region: equivalent sufficient qualification model
 skill_set |> qa$sqa(df_occupations[-1], aeq_method = "linear-logistic")
-# skill_set |> qa$sqa(as.matrix(df_occupations[-1]), aeq_method = "linear-logistic")
+df_occupations[1:3] |> qa$sqa(df_occupations[1:19], aeq_method = "linear-logistic")
 
 # endregion
