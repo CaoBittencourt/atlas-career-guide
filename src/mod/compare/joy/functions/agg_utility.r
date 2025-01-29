176c95573db90a1f6e823d070e204a12f1231abe
Cao Bittencourt <caobittencourt@gmail.com>
2025-01-29 19:30:23 -0300

    linear utility aggregation working well

--- src/mod/compare/joy/functions/agg_utility.r
+++ src/mod/compare/joy/functions/agg_utility.r
@@ -12,13 +12,50 @@ box::use(
 )
 
 # endregion
+# # region: CES utility aggregation
+# bin.ces <- function(uk, aq) {
+#   # uk |> ugene() -> ugenek
+#   uk / sum(uk) -> ũk
+#   1 / (1 - (1 - ugene(uk))) -> es
+
+#   # ces utility aggregator
+#   return(
+#     sum(
+#       (ũk^(1 / es)) *
+#         (aq^((es - 1) / es))
+#     )^(
+#       es / (es - 1)
+#     )
+#   )
+#   # sum((ũk^(1 / s)) * (u(uk, aq)^((s - 1) / s)))^(s / (s - 1))
+
+#   # # perfect substitutes when s -> Inf
+#   # # perfect complements when s -> 0
+
+#   # # ugene -> 1 => utility generalist => perfect substitutes => s -> inf
+#   # # ugene -> 0 => utility specialist => perfect complements => s -> 0
+#   # s := f(ugene) | ugene -> 1 => f(ugene) -> Inf, ugene -> 0 => f(ugene) -> 0
+#   # 1 / (1 - ugene)
+#   # f(1) = Inf
+#   # f(0) = 0
+
+#   # U <- (
+#   #   sum(
+#   #     (a^(1 / s)) * (x^((s - 1) / s))
+#   #   )
+#   # )^(
+#   #   s / (s - 1)
+#   # )
+# }
+
+# # endregion
 # region: CES utility aggregation
-bin.ces <- function(uk, aq
-                    # , ük
-) {
+bin.ces <- function(uk, aq) {
   # uk |> ugene() -> ugenek
   uk / sum(uk) -> ũk
   1 / (1 - ugene(uk)) -> es
+
+  1 + es -> es
 
   # ces utility aggregator
   return(
@@ -143,33 +180,78 @@ agg.utility <- function(pref_set, skill_mtx, agg.method = c("linear", "concave",
 
 # endregion
 # region: tests
+library(dplyr)
+library(tidyr)
 getOption("atlas.skills_mtx") |>
   readRDS() |>
   dplyr::select(-1) ->
 dsds
 
-box::use(mod / utils / vmap)
-dsds |> vmap$vmap(dsds, bin.ces) -> lalala
-agg.utility(dsds[1:2], dsds[1:3], agg.method = c("linear", "concave", "convex"), util.fn = function(uk, ak) uk, bind = F) -> lalala
-library(dplyr)
-library(tidyr)
-lalala |>
-  as_tibble(
-    rownames = "to"
+box::use(mod / utils / logistic)
+getOption("atlas.cao") |>
+  readRDS() |>
+  select(-1) |>
+  agg.utility(
+    getOption("atlas.skills") |>
+      readRDS() |>
+      inner_join(
+        getOption("atlas.cao") |>
+          readRDS() |>
+          select(item)
+      ) |>
+      pivot_wider(
+        names_from = occupation,
+        values_from = item_score
+      ),
+    util.fn = function(uk, aq) {
+      # 1 - (2 * aq - uk)^2
+      # logistic$logistic(aq, 0, 1, 1, 1, uk, 1, 1)
+      logistic$logistic(aq, 0, uk, 1, 1, uk, 1, 1)
+    }
   ) |>
-  pivot_longer(
-    cols = -1,
-    names_to = "from",
-    values_to = "utility"
+  arrange(desc(cao))
+# arrange(cao)
+
+getOption("atlas.skills") |>
+  readRDS() |>
+  inner_join(
+    getOption("atlas.cao") |>
+      readRDS()
   ) |>
-  group_by(from) |>
-  arrange(
-    -utility,
-    .by_group = T
+  group_by(
+    occupation
   ) |>
-  group_split()
+  reframe(
+    utility = bin.ces(
+      uk = cao,
+      aq = item_score
+    )
+  ) |>
+  arrange(desc(
+    utility
+  ))
 
 
+# box::use(mod / utils / vmap)
+# dsds |> vmap$vmap(dsds, bin.ces) -> lalala
+# agg.utility(dsds[1:2], dsds[1:3], agg.method = c("linear", "concave", "convex"), util.fn = function(uk, ak) uk, bind = F) -> lalala
+# library(dplyr)
+# library(tidyr)
+# lalala |>
+#   as_tibble(
+#     rownames = "to"
+#   ) |>
+#   pivot_longer(
+#     cols = -1,
+#     names_to = "from",
+#     values_to = "utility"
+#   ) |>
+#   group_by(from) |>
+#   arrange(
+#     -utility,
+#     .by_group = T
+#   ) |>
+#   group_split()
 
 # endregion
 # # region: exports
