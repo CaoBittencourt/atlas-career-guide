3bb5071d2dcd19119b662e37e01e2f65a8706286
Cao Bittencourt <caobittencourt@gmail.com>
2025-02-26 14:47:41 -0300

    misc

--- tests/misc.r
+++ tests/misc.r
@@ -3,105 +3,198 @@
 library(ipeadatar)
 library(lubridate)
 library(stringr)
+library(readr)
 library(dplyr)
 library(tidyr)
 
 # endregion
 # region: data
 # rents table
-tibble(
-  "rentId" = 1:2 |> as.factor(),
-  "apt" = c(301, 405) |> as.factor(),
-  "rentBonified" = c(1500, 1100)
-) -> rents
+read.csv(
+  url(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMtreiEOjVAORxhEwcZk5DIJC1XEI5t3Q2FzXlHbTxGwx_ZCrjsWJ35igbku2NbXn1lfBfsSh7E6fR/pub?gid=670718951&single=true&output=csv"
+  ),
+  sep = ",",
+  dec = ","
+) |>
+  as_tibble() ->
+rents
 
-# dates and discounts table
-tibble(
-  "hazardId" = 1:2 |> as.factor(),
-  "type" = c("cupim", "infiltração") |> as.factor(),
-  "apt" = c(301, 405) |> as.factor(),
-  "desc" = c("dsds", "lalala"),
-  "dateNotified" = c("2025-01-01", "2024-07-01") |> as.Date(),
-  "dateSolved" = c("2025-03-01", "2025-03-01") |> as.Date(),
-  "discount" = c(.05, .1),
-) -> hazards
+# recurring hazards table
+read.csv(
+  url(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMtreiEOjVAORxhEwcZk5DIJC1XEI5t3Q2FzXlHbTxGwx_ZCrjsWJ35igbku2NbXn1lfBfsSh7E6fR/pub?gid=0&single=true&output=csv"
+  ),
+  sep = ",",
+  dec = ","
+) |>
+  as_tibble() |>
+  mutate(
+    across(
+      .cols = starts_with("date"),
+      .fns = as.Date,
+      format = "%d/%m/%Y"
+    ),
+    dateSolved = if_else(
+      is.na(dateSolved),
+      today(),
+      dateSolved
+    )
+  ) ->
+hazardsRecurring
 
+# lumpsum (non recurring) hazards table
+read.csv(
+  url(
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMtreiEOjVAORxhEwcZk5DIJC1XEI5t3Q2FzXlHbTxGwx_ZCrjsWJ35igbku2NbXn1lfBfsSh7E6fR/pub?gid=1650970469&single=true&output=csv"
+  ),
+  sep = ",",
+  dec = ","
+) |>
+  as_tibble() |>
+  mutate(
+    across(
+      .cols = starts_with("date"),
+      .fns = as.Date,
+      format = "%d/%m/%Y"
+    ),
+    dateEnd = if_else(
+      is.na(dateEnd),
+      today(),
+      dateEnd
+    )
+  ) ->
+hazardsLumpsum
 
+# tibble(
+#   "hazardId" = 1:2 |> as.factor(),
+#   "type" = c("cupim", "infiltração") |> as.factor(),
+#   "apt" = c(301, 405) |> as.factor(),
+#   "desc" = c("dsds", "lalala"),
+#   "dateNotified" = c("2025-01-01", "2024-07-01") |> as.Date(),
+#   "dateSolved" = c("2025-03-01", "2025-03-01") |> as.Date(),
+#   "discount" = c(.05, .1),
+# ) -> hazards
 
-# inflation rate
-# ipeadatar::available_series() |>
-#   mutate(across(
-#     .cols = -where(is.numeric),
-#     .fns = str_to_lower
-#   )) |>
-#   filter(
-#     name |> str_detect("ipca") &
-#       freq == "monthly" &
-#       status == "active"
-#   ) |>
-#   arrange(desc(
-#     lastupdate
-#   )) -> inflationSeries
-
-"precos12_ipcag12" |>
-  str_to_upper() |>
-  ipeadata() |>
+# risk-free interest rate (selic)
+read.csv(
+  url(
+    paste0(
+      "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=csv&dataInicial=",
+      min(
+        hazardsRecurring$dateNotified |> min(na.rm = T),
+        hazardsLumpsum$dateStart |> min(na.rm = T)
+      ) |> format(format = "%d/%m/%Y"),
+      "&dataFinal=",
+      max(
+        hazardsRecurring$dateSolved |> max(na.rm = T),
+        hazardsLumpsum$dateEnd |> max(na.rm = T)
+      ) |> format(format = "%d/%m/%Y")
+    )
+  ),
+  sep = ";",
+  dec = ","
+) |>
+  as_tibble() |>
   rename(
-    inflation = value
-  ) |>
-  select(
-    date,
-    inflation
-  ) |>
-  filter(
-    date >=
-      hazards$
-        dateNotified |>
-        min() - months(1)
-  ) |>
-  filter(
-    date <=
-      hazards$
-        dateSolved |>
-        max()
+    date = 1,
+    interestRate = 2
   ) |>
   mutate(
-    inflation = 1 + inflation / 100
-  ) |>
-  slice(-1) ->
-inflation
+    across(
+      .cols = starts_with("date"),
+      .fns = as.Date,
+      format = "%d/%m/%Y"
+    ),
+    interestRate = 1 + interestRate / 100,
+  ) ->
+interestRate
+
+# interestRate |>
+#   reframe(
+#     timeDiff =
+#       as.integer(
+#         max(date) - min(date)
+#       ),
+#     timeNrowDiff =
+#       timeDiff - n()
+#   )
+
+# # inflation rate
+# # ipeadatar::available_series() |>
+# #   mutate(across(
+# #     .cols = -where(is.numeric),
+# #     .fns = str_to_lower
+# #   )) |>
+# #   filter(
+# #     # name |> str_detect("ipca") &
+# #     name |> str_detect("selic") &
+# #       freq == "monthly" &
+# #       status == "active"
+# #   ) |>
+# #   arrange(desc(
+# #     lastupdate
+# #   )) -> inflationSeries
+
+# "precos12_ipcag12" |>
+#   str_to_upper() |>
+#   ipeadata() |>
+#   rename(
+#     inflation = value
+#   ) |>
+#   select(
+#     date,
+#     inflation
+#   ) |>
+#   filter(
+#     date >=
+#       hazards$
+#         dateNotified |>
+#         min() - months(1)
+#   ) |>
+#   filter(
+#     date <=
+#       hazards$
+#         dateSolved |>
+#         max()
+#   ) |>
+#   mutate(
+#     inflation = 1 + inflation / 100
+#   ) |>
+#   slice(-1) ->
+# inflation
 
 # endregion
 # model
-# region: parameters
-
-# endregion
-# region: compensation
-hazards |>
+# region: recurring (discount) compensation
+hazardsRecurring |>
   group_by(hazardId) |>
   mutate(
-    monthDiff =
-      dateNotified |>
-        interval(
-          dateSolved
-        ) %/% months(1) |>
-        as.integer()
+    dateDiff =
+      as.integer(
+        dateSolved - dateNotified
+      )
+  ) |>
+  filter(
+    dateDiff |>
+      is.na() |>
+      isFALSE()
   ) |>
   slice(
-    rep(1, monthDiff)
+    rep(1, dateDiff)
   ) |>
   mutate(
-    date = dateNotified + months(row_number() - 1)
+    date = dateNotified + days(row_number() - 1)
   ) |>
   ungroup() |>
   inner_join(
-    inflation,
+    interestRate,
     multiple = "all"
   ) |>
   group_by(hazardId) |>
   mutate(
-    inflationCum =
-      inflation |>
+    interestCum =
+      interestRate |>
         rev() |>
         cumprod() |>
         rev()
@@ -114,12 +207,16 @@ hazards |>
     compensation =
       rentBonified *
         discount *
-        inflationCum
+        interestCum
   ) |>
   select(
     -rentId,
     -rentBonified
-  ) ->
+  ) |>
+  pull(
+    interestCum
+  ) |>
+  max()
 hazards
 
 hazards
@@ -132,40 +229,59 @@ hazards |>
   )
 
 # endregion
-# # exp(.01)
-# # 1.01^12
-# # log(1.12)
+# # region: compensation
+# hazards |>
+#   group_by(hazardId) |>
+#   mutate(
+#     monthDiff =
+#       dateNotified |>
+#         interval(
+#           dateSolved
+#         ) %/% months(1) |>
+#         as.integer()
+#   ) |>
+#   slice(
+#     rep(1, monthDiff)
+#   ) |>
+#   mutate(
+#     date = dateNotified + months(row_number() - 1)
+#   ) |>
+#   ungroup() |>
+#   inner_join(
+#     inflation,
+#     multiple = "all"
+#   ) |>
+#   group_by(hazardId) |>
+#   mutate(
+#     inflationCum =
+#       inflation |>
+#         rev() |>
+#         cumprod() |>
+#         rev()
+#   ) |>
+#   inner_join(
+#     rents,
+#     multiple = "all"
+#   ) |>
+#   mutate(
+#     compensation =
+#       rentBonified *
+#         discount *
+#         inflationCum
+#   ) |>
+#   select(
+#     -rentId,
+#     -rentBonified
+#   ) ->
+# hazards
 
-# # sum(1.01^(0:8) * 1500 * .3) * 8 + sum(1.01^(0:11) * (240 - 90)) * 18
-# # sum(1.01^(0:8) * 1500 * .3) + sum(1.01^(0:11) * (240 - 90))
-# # sum(1.01^(0:11) * (240 - 90))
-# # sum(exp(0.1133287 * 0:8 / 12) * 1500 * .3) * 8 + sum(exp(0.1133287 * 0:11 / 12) * (240 - 90)) * 18
+# hazards
 
-# # sum(exp(0.1133287 * 0:8 / 12) * 1100 * .5) + sum(exp(0.1133287 * 0:11 / 12) * (240 - 90))
+# hazards |>
+#   group_by(apt) |>
+#   reframe(
+#     compensationTotal =
+#       sum(compensation)
+#   )
 
-# # monthly interest rate
-# ipca <- 1.0968
-# log(ipca) / 12 -> i
-
-# # affected time periods (months)
-# t <- 9
-
-# # total time periods (months)
-# tt <- 12
-
-# # average rent (brl)
-# rent <- 1500
-
-# # average discount on rent (%)
-# discount <- .25
-
-# # average net condominium fee (minus baseline water expenditure = 90)
-# condo.fee <- 240 - 90
-
-# # total number of households
-# n <- 18
-
-# # number of affected households (8?)
-# m <- 8
-
-# sum(exp(i * 0:(t - 1)) * rent * discount) * m + sum(exp(i * 0:(tt - 1)) * condo.fee) * n
+# # endregion
