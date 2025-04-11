dd4a234134728d03b2688817467514d770336084
Cao Bittencourt <caobittencourt@gmail.com>
2025-04-11 19:17:46 -0300

    dijkstra career path sketch is working

--- models/roadmap/igraph.r
+++ models/roadmap/igraph.r
@@ -46,7 +46,7 @@ career.grid <- function(xmin, tmin, xmax = NULL, tmax = education$doctorate) {
 # basic education
 career.grid(0, 0, education$high.school, xmax = 0) -> basic.education
 
-basic.education
+# basic.education
 
 # minimum required education
 "atlas.education" |>
@@ -98,8 +98,8 @@ c(
   list(basic.education)
 ) -> career.grids
 
-career.grids[[1]]
-career.grids[[length(career.grids)]]
+# career.grids[[1]]
+# career.grids[[length(career.grids)]]
 
 # similarity matrix
 career.req |>
@@ -132,6 +132,22 @@ career.req |>
   ) ->
 mtx_similarity
 
+mtx_similarity |> mutate(`Basic Education` = 1) -> mtx_similarity
+
+mtx_similarity |>
+  bind_rows(
+    mtx_similarity |>
+      slice(1) |>
+      mutate(
+        to = "Basic Education",
+        across(
+          .cols = where(is.numeric),
+          .fns = ~1
+        )
+      )
+  ) ->
+mtx_similarity
+
 # career move (note: movement within the same career uses this function as well)
 career.move <- function(skq, xk, xq, tk, tq) {
   # remove baseline education
@@ -158,66 +174,63 @@ career.move <- function(skq, xk, xq, tk, tq) {
   return((req.x + req.t) / skq.eq)
 }
 
-# example: movement within the same occupation
-career.grids[[1]][1, ] -> from
-career.grids[[1]][2, ] -> to
-career.move(1, from$x, to$x, from$t, to$t)
+# # example: movement within the same occupation
+# career.grids[[1]][1, ] -> from
+# career.grids[[1]][2, ] -> to
+# career.move(1, from$x, to$x, from$t, to$t)
 
-# example: movement within the same occupation
-career.grids[[1]][1, ] -> from
-career.grids[[1]][3, ] -> to
-career.move(1, from$x, to$x, from$t, to$t)
+# # example: movement within the same occupation
+# career.grids[[1]][1, ] -> from
+# career.grids[[1]][3, ] -> to
+# career.move(1, from$x, to$x, from$t, to$t)
 
-# example: career switch
-career.grids[[length(career.grids)]][2, ] -> from
-career.grids[[1]][1, ] -> to
-career.move(
-  1,
-  from$x,
-  to$x,
-  from$t,
-  to$t
-)
+# # example: career switch
+# career.grids[[length(career.grids)]][2, ] -> from
+# career.grids[[1]][1, ] -> to
+# career.move(
+#   1,
+#   from$x,
+#   to$x,
+#   from$t,
+#   to$t
+# )
 
-# example: career switch
-career.grids[[1]][1, ] -> from
-career.grids[[3]][1, ] -> to
-mtx_similarity |>
-  select(-1) |>
-  slice(3) |>
-  pull(1) |>
-  career.move(
-    from$x,
-    to$x,
-    from$t,
-    to$t
-  )
+# # example: career switch
+# career.grids[[1]][1, ] -> from
+# career.grids[[3]][1, ] -> to
+# mtx_similarity |>
+#   select(-1) |>
+#   slice(3) |>
+#   pull(1) |>
+#   career.move(
+#     from$x,
+#     to$x,
+#     from$t,
+#     to$t
+#   )
 
-# example: career switch
-career.grids[[1]][1, ] -> from
-career.grids[[2]][1, ] -> to
-mtx_similarity |>
-  select(-1) |>
-  slice(2) |>
-  pull(1) |>
-  career.move(
-    from$x,
-    to$x,
-    from$t,
-    to$t
-  )
+# # example: career switch
+# career.grids[[1]][1, ] -> from
+# career.grids[[2]][1, ] -> to
+# mtx_similarity |>
+#   select(-1) |>
+#   slice(2) |>
+#   pull(1) |>
+#   career.move(
+#     from$x,
+#     to$x,
+#     from$t,
+#     to$t
+#   )
 
-career.grids |>
-  bind_rows() |>
-  nrow() |>
-  choose(2)
-
+# vertices
 career.grids |>
   bind_rows(
     .id = "occupation"
   ) |>
   mutate(
     .before = 1,
+    occupation = as.integer(occupation),
     vertex = row_number()
   ) ->
 vertices
@@ -225,8 +238,7 @@ vertices
 expand.grid(
   from = vertices$vertex,
   to = vertices$vertex
-) |>
-  filter(from != to) ->
+) ->
 vertices.combn
 
 vertices -> vertices.from
@@ -246,76 +258,192 @@ vertices.combn |>
   ) ->
 vertices.combn
 
-vertices.combn |>
-  filter(!(
-    occupation.from == occupation.to &
-      x.from >= x.to & t.from >= t.to
-  )) ->
+# vertices.combn |>
+#   filter(!(
+#     occupation.from == occupation.to &
+#       x.from >= x.to & t.from >= t.to
+#   )) ->
+# vertices.combn
+
+mtx_similarity |>
+  mutate(
+    id = row_number(),
+    occupation = to
+  ) |>
+  select(
+    id,
+    occupation
+  ) ->
+ids
+
+mtx_similarity |>
+  pivot_longer(
+    cols = -1,
+    names_to = "from",
+    values_to = "similarity"
+  ) |>
+  inner_join(
+    ids |> rename(id.to = id),
+    by = c(
+      "to" = "occupation"
+    )
+  ) |>
+  inner_join(
+    ids |> rename(id.from = id),
+    by = c(
+      "from" = "occupation"
+    )
+  ) |>
+  select(
+    occupation.to = id.to,
+    occupation.from = id.from,
+    similarity
+  ) |>
+  right_join(
+    vertices.combn,
+    relationship = "many-to-many",
+    multiple = "all",
+  ) ->
 vertices.combn
 
-# adjency matrix
-cbind(
-  a = c(0, 19, 1, 9),
-  b = c(19, 0, 9, 1),
-  c = c(9, 19, 0, 1),
-  d = c(9, 1, 19, 0)
-) -> adj
+vertices.combn |>
+  mutate(
+    cost = career.move(
+      similarity,
+      x.from,
+      x.to,
+      t.from,
+      t.to
+    )
+  ) ->
+vertices.combn
 
-cbind(
-  a = c(0, 19, 1, 9),
-  b = c(19, 0, 9, 1),
-  c = c(9, 19, 0, 1),
-  d = c(9, 1, 19, 0)
-) *
-  cbind(
-    a = 1 - rep(1, 4),
-    b = 1 - rep(0.19, 4),
-    c = 1 - rep(.5, 4),
-    d = 1 - rep(.19, 4)
-  ) -> wgt
+vertices.combn |>
+  mutate(
+    cost = ifelse(
+      is.na(cost) & similarity < 0.5,
+      Inf,
+      cost
+    )
+  ) ->
+vertices.combn
 
-colnames(adj) -> rownames(adj)
-colnames(wgt) -> rownames(wgt)
+# vertices.combn |>
+#   filter(
+#     !is.infinite(cost)
+#   ) ->
+# vertices.combn
 
-adj
-wgt
-
-# graph struct
-adj |>
+vertices.combn |>
+  select(
+    from,
+    to,
+    cost
+  ) |>
+  pivot_wider(
+    id_cols = "from",
+    names_from = "to",
+    values_from = "cost"
+  ) |>
+  select(-1) |>
+  as.matrix() |>
   gr$graph.adjacency(
     mode = "directed",
     weighted = T
   ) ->
-x
+career.graph
 
-wgt |>
-  gr$graph.adjacency(
-    mode = "directed",
-    weighted = T
-  ) ->
-x
+# # adjency matrix
+# cbind(
+#   a = c(0, 19, 1, 9),
+#   b = c(19, 0, 9, 1),
+#   c = c(9, 19, 0, 1),
+#   d = c(9, 1, 19, 0)
+# ) -> adj
+
+# cbind(
+#   a = c(0, 19, 1, 9),
+#   b = c(19, 0, 9, 1),
+#   c = c(9, 19, 0, 1),
+#   d = c(9, 1, 19, 0)
+# ) *
+#   cbind(
+#     a = 1 - rep(1, 4),
+#     b = 1 - rep(0.19, 4),
+#     c = 1 - rep(.5, 4),
+#     d = 1 - rep(.19, 4)
+#   ) -> wgt
+
+# colnames(adj) -> rownames(adj)
+# colnames(wgt) -> rownames(wgt)
+
+# adj
+# wgt
+
+# # graph struct
+# adj |>
+#   gr$graph.adjacency(
+#     mode = "directed",
+#     weighted = T
+#   ) ->
+# x
+
+# wgt |>
+#   gr$graph.adjacency(
+#     mode = "directed",
+#     weighted = T
+#   ) ->
+# x
 
 # shortest path
-x |>
+career.graph |>
   gr$shortest_paths(
-    from = (x |> V() |> names() == "a") |> which(),
-    to = (x |> V() |> names() == "b") |> which(),
+    from = vertices.combn |>
+      select(
+        occupation.from,
+        from
+      ) |>
+      filter(
+        occupation.from == 874
+      ) |>
+      unique() |>
+      pull(from) |>
+      min(),
+    to = vertices.combn |>
+      select(
+        occupation.from,
+        from
+      ) |>
+      filter(
+        occupation.from == 1
+      ) |>
+      unique() |>
+      pull(from) |>
+      min(),
     algorithm = "dijkstra",
     output = "epath"
-  ) ->
-xpath
-
-# # shortest path
-# x |>
-#   distances(
-#     v = (x |> V() |> names() == "a") |> which(),
-#     to = V(x),
-#     algorithm = "dijkstra"
-#   )
+  )
 
 # x |>
-#   distances(
-#     v = (x |> V() |> names() == "a") |> which(),
+#   gr$shortest_paths(
+#     from = (x |> V() |> names() == "a") |> which(),
 #     to = (x |> V() |> names() == "b") |> which(),
-#     algorithm = "dijkstra"
-#   )
+#     algorithm = "dijkstra",
+#     output = "epath"
+#   ) ->
+# xpath
+
+# # # shortest path
+# # x |>
+# #   distances(
+# #     v = (x |> V() |> names() == "a") |> which(),
+# #     to = V(x),
+# #     algorithm = "dijkstra"
+# #   )
+
+# # x |>
+# #   distances(
+# #     v = (x |> V() |> names() == "a") |> which(),
+# #     to = (x |> V() |> names() == "b") |> which(),
+# #     algorithm = "dijkstra"
+# #   )
