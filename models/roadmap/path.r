8f3574a6367247d1cef311a8b0b9ee23628f89f2
Cao Bittencourt <caobittencourt@gmail.com>
2025-05-02 13:20:03 -0300

    tests working

--- models/roadmap/path.r
+++ models/roadmap/path.r
@@ -63,12 +63,16 @@ pa$paths$table |>
   arrange(x, t) ->
 vertices.to
 
+index.from <- 1
+index.to <- 1
+
 # find path
 pa$path(
-  from = vertices.from$vertex[1],
-  to = vertices.to$vertex[1]
+  from = vertices.from$vertex[index.from],
+  to = vertices.to$vertex[index.to]
 ) -> path.actor_accountant
 
+# career path
 occupations[
   pa$paths$table |>
     slice(
@@ -76,5 +80,86 @@ occupations[
     ) |>
     pull(occupation)
 ]
+
+# verify path is optimal
+if (
+  path.actor_accountant |>
+    pa$path.cost() |>
+    sum() <=
+    sum(
+      vertices.to$x[index.to],
+      vertices.to$t[index.to]
+    )
+) {
+  print("Path is optimal.")
+} else {
+  print("Path is not optimal.")
+}
+
+# endregion
+# region: accountant => actor
+# get vertices
+pa$paths$table |>
+  filter(
+    occupation ==
+      occupations$`Accountants and Auditors`
+  ) |>
+  select(
+    occupation,
+    vertex,
+    x,
+    t
+  ) |>
+  unique() |>
+  arrange(x, t) ->
+vertices.from
+
+pa$paths$table |>
+  filter(
+    occupation ==
+      occupations$Actors
+  ) |>
+  select(
+    occupation,
+    vertex,
+    x,
+    t
+  ) |>
+  unique() |>
+  arrange(x, t) ->
+vertices.to
+
+index.from <- 1
+index.to <- 10
+
+# find path
+pa$path(
+  from = vertices.from$vertex[index.from],
+  to = vertices.to$vertex[index.to]
+) -> path.actor_accountant
+
+# career path
+occupations[
+  pa$paths$table |>
+    slice(
+      path.actor_accountant
+    ) |>
+    pull(occupation)
+]
+
+# verify path is optimal
+if (
+  path.actor_accountant |>
+    pa$path.cost() |>
+    sum() <=
+    sum(
+      vertices.to$x[index.to],
+      vertices.to$t[index.to]
+    )
+) {
+  print("Path is optimal.")
+} else {
+  print("Path is not optimal.")
+}
 
 # endregion
