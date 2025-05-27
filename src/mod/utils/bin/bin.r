be96862a2999d12b9c48578d5bb65f2e5ee6afd6
Cao Bittencourt <caobittencourt@gmail.com>
2025-05-27 17:00:57 -0300

    bins

---
+++ src/mod/utils/bin/bin.r
@@ -0,0 +1,43 @@
+# setup
+# region: imports
+box::use(
+  assert = mod / utils / assert,
+)
+
+# endregion
+# methods
+# region: pdf method
+bin.pdf <- function(x, bins, ...) {
+  # assert args in main function
+  # description
+  return("pdf")
+}
+
+# endregion
+# region: default method
+bin.default <- function(x, bins, ...) {
+  # assert args in main function
+  # description
+  return("default")
+}
+
+# endregion
+# dispatch
+# region: bin generic function
+bin <- function(x, bins, ...) {
+  # assert args
+
+  # multiple dispatch
+  if (x |> is.function()) {
+    return(bin.pdf(x, bins, ...))
+  }
+
+  return(bin.default(x, bins, ...))
+}
+
+# endregion
+# exports
+# region: exports
+box::export(bin, bin.methods)
+
+# endregion
