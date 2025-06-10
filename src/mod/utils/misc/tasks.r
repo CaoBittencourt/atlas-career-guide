f93767b27c1c11ca03376834d4273c83f034889d
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-09 21:35:54 -0300

    indentation parser

--- src/mod/utils/misc/tasks.r
+++ src/mod/utils/misc/tasks.r
@@ -3,283 +3,76 @@ modular::project.options("atlas")
 box::use(
   dplyr[...],
   tidyr[...],
-  rs2 = reshape2,
+  str = stringr,
+  ig = igraph,
 )
 
-list(
-  prog = list(
-    analytic = list(
-      math = list(
-        recursive = NA
-      ) # status = value, desc = name
-    ),
-    dijkstra = list(
-      path.mod = list(
-        movement.type = list(
-          switch = 1,
-          work = 1,
-          study = 1,
-          reset = 1
-        ),
-        movement.prob = list(
-          indie.dist = 1,
-          joint.dist = 0.5
-        )
-      )
-    )
-  ),
-  dsds = list(
-    NA
-  ),
-  lalala = list(
-    analytic = list(
-      math = list(
-        recursive = NA
-      ) # status = value, desc = name
-    ),
-    dijkstra = list(
-      path.mod = list(
-        movement.type = list(
-          switch = 1,
-          work = 1,
-          study = 1,
-          reset = 1
-        )
-      )
-    )
-  ),
-  prog = list(
-    analytic = list(
-      math = list(
-        recursive = NA
-      ) # status = value, desc = name
-    )
-  ),
-  prog = list(
-    NA
-  )
-) -> tasks
+parse.tasks <- function(path) {
+  path |>
+    readLines(warn = F) |>
+    str$str_trim("right") ->
+  tasks
 
-count.recursive <- function(x){
-  if(!is.list(x)){
-    return(seq_along(x))
-  }
+  tasks |> str$str_count("  ") -> indentation
+  indentation / ifelse(all((indentation %% 2) == 0), 2, 1) -> indentation
+  indentation |> as.integer() -> indentation
 
-  return(
-    x |> unlist() |> lapply(count.recursive)
-  )
+  data.frame(
+    title = tasks |> str$str_trim(),
+    task = indentation |> seq_along(),
+    nest = indentation
+  ) ->
+  tasks.lines
 
-}
+  tasks.lines$title |> str$str_split(":", 2, T) -> tasks
 
-tasks |> count.recursive()
+  tasks[, 1] |> str$str_trim() -> tasks.lines$title
+  tasks[, 2] |> str$str_trim() -> tasks.lines$status
+  tasks.lines$status[tasks.lines$status == ""] <- "0"
+  tasks.lines$status |> as.numeric() -> tasks.lines$status
 
-tasks
+  tasks.lines$isProj <- tasks.lines$nest == 0
+  tasks.lines$proj <- cumsum(tasks.lines$isProj)
 
-recursive.id <- function(x){
-  if(!is.list(x)){
-    return(x)
-  }
+  mapply(
+    function(n, l) {
+      max(
+        tasks.lines$task[
+          (
+            tasks.lines$nest + ifelse(
+              tasks.lines$task < n, 0, Inf
+            )
+          )
+          < l
+        ]
+      ) -> id
 
-  return(
-    x |> lapply(recursive.id)
-  )
-}
+      id[is.infinite(id)] <- n
 
-
-tasks$prog$analytic$math$recursive
-tasks$prog$dijkstra$path.mod$movement.type$switch
-tasks$prog$dijkstra$path.mod$movement.type$work
-tasks$prog$dijkstra$path.mod$movement.type$study
-tasks$prog$dijkstra$path.mod$movement.type$reset
-tasks$prog$dijkstra$path.mod$movement.prob$indie.dist
-tasks$prog$dijkstra$path.mod$movement.prob$joint.dist
-tasks$dsds
-tasks$lalala$analytic$math$recursive
-tasks$lalala$dijkstra$path.mod$movement.type$switch
-tasks$lalala$dijkstra$path.mod$movement.type$work
-tasks$lalala$dijkstra$path.mod$movement.type$study
-tasks$lalala$dijkstra$path.mod$movement.type$reset
-tasks$prog$analytic$math$recursive
-tasks$prog$dijkstra$path.mod$movement.type$switch
-tasks$prog$dijkstra$path.mod$movement.type$work
-tasks$prog$dijkstra$path.mod$movement.type$study
-tasks$prog$dijkstra$path.mod$movement.type$reset
-tasks$prog$dijkstra$path.mod$movement.prob$indie.dist
-tasks$prog$dijkstra$path.mod$movement.prob$joint.dist
-
-tasks |> rapply()
-
-tasks
-
-na.string <- '<NA>'
-tasks |> rs2$melt(value.name = 'status') -> tasks.nested
-tasks.nested[,
-  tasks.nested |> colnames() |> order()
-] -> tasks.nested
-
-tasks.nested
-
-tasks.nested |> 
-  mutate(
-    projId = as.factor.chr(L1)
-  )
-
-
-
-tasks.nested |> 
-  mutate(
-    taskId = row_number()
-  ) |> 
-  pivot_longer(
-    cols = -c(status, taskId),
-    names_to = 'level',
-    values_to = 'task'
-  ) |> 
-  mutate(
-    level = 
-      level |> 
-      extract_numeric(),
-    task = 
-      if_else(
-    task |> extract_numeric() |> is.na(),
-    task,
-    ''
-      )
-  ) |> 
-  filter(
-    task != na.string
+      return(id)
+    },
+    n = tasks.lines$task,
+    l = tasks.lines$nest
   ) |>
-  group_by(level == 1) |> 
-  arrange(level, .by_group = T) |> 
-  mutate(
-    levelId = 
-  )
-  group_by(taskId) |> 
-  arrange(level, .by_group = T) |> 
-  mutate(
-    from = lag(task),
-    to = task
-  )
+    as.numeric() ->
+  tasks.lines$from
 
-tasks.df$level |> extract_numeric()
-
-filter(!(level |> is.na()))
-
-tasks |> 
-  flatten_dfc() |> 
-  pivot_longer(
-  cols = everything(),
-  names_to = 'task',
-  values_to = 'status'
-) -> task.values
-
-task.values$task -> task.names
-
-tasks |> reshape2::melt()
-
-tasks |> lapply(names) -> dsds
-
-# Map(
-#   function(x, x.lag){
-
-#   },
-#   x = tasks |> names(),
-# )
-
-tasks |> lapply(names) |> names()
-tasks |> nestmap(names)
-tasks |> nestmap(nestmap, names)
-tasks |> nestmap(nestmap, nestmap, names)
-tasks |> nestmap(nestmap, nestmap, nestmap, nestmap, names)
-tasks |> nestmap(nestmap, nestmap, nestmap, nestmap, nestmap, names)
-
-
-names(tasks)[which(names(tasks) %in% task.names)]
-
-task.names
-
-
-tasks |> lapply(names)
-
-
-'analytic' %in% names(tasks)
-'analytic' %in% names(tasks$prog)
-
-tasks
-
-tasks
-
-tasks$dsds |> names()
-
-
-task.names |> lapply(function(x) tasks[[x]])
-tasks
-
-tasks |> recursive.names() -> dsds
-
-dsds$prog$analytic$math |> class()
-dsds$prog$dijkstra$path.mod$movement.type
-task.values
-task.names
-recursive.names <- function(x){
-  if(is.list(x[[1]])){
-    return(
-      x |> lapply(recursive.names)
+  tasks.lines[
+    c(
+      "title",
+      "proj",
+      "task",
+      "from",
+      "nest",
+      "status"
     )
-  }
+  ] ->
+  tasks.lines
 
   return(
-    unlist(x)
+    tasks.lines
   )
 }
 
-tasks |> 
-task.names[1]
 
-tasks
-
-tasks
-
-
-tasks$prog$analytic$math$recursive
-
-recursive.names <- function(x){
-  if(!is.list(x)){
-    return(x)
-  }
-
-  return(
-    x |> bind_rows(.id = 'task')
-  )
-}
-
-tasks |> recursive.names()
-
-tasks$prog$analytic$math$recursive
-tasks |> names() |> unlist() |> names() |> stringr::str_split('\\.') -> tasks.path
-
-tasks.path |> lapply(lag) |> lapply(last, na_rm = T)
-
-
-
-tasks |> list_flatten()
-
-tasks |> lapply(function(tasks) tasks |> bind_rows(.id = 'task'))
-
-tasks |> list() |> data.table::rbindlist(fill = T)
-tasks |> list() |> purrr::map_dfr(as_tibble)
-
-unlist.df <- function(x){
-  if(!is.list(x)){
-    return(x)
-  }
-
-  return(x |> bind_rows(.id = 'dsds') |> unlist.df())
-}
-
-tasks |> unlist.df() -> dsdsds
-tasks |> bind_rows(.id = 'dsds')
-
-tasks |> unlist(recursive = F, use.names = T) -> dsds
-
-dsds$
+parse.tasks("/home/Cao/storage/github/atlas/src/mod/utils/misc/dsds.txt")
