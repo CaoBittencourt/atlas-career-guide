bc85554250e31e58ca7e39e6879e69ccb7155df9
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-24 17:29:42 -0300

    setup python dev environment

---
+++ src/mod/utils/misc/standup.r
@@ -0,0 +1,41 @@
+box::use(
+  clipr[...],
+  dplyr[...]
+)
+
+sample(
+  c(
+    "Caike Ferreira",
+    "Elmer Ribeiro",
+    "Jamile Queiroz",
+    "Lucas Chinaglia de Oliveira",
+    "Milena Weidgenant e Silva",
+    "Tatiana Frigoletto",
+    "Uelinton Santos",
+    "Aline Contente",
+    "Anderson Gomes da Silva",
+    "Cao Bittencourt",
+    "Felipe Fittipaldi",
+    "Joao Lima",
+    "Leticia Scalioni"
+  )
+) |>
+  as_tibble() |>
+  rename(
+    name = 1
+  ) |>
+  mutate(
+    order = paste0(
+      row_number(),
+      ". ",
+      name
+    )
+  ) |>
+  reframe(
+    order |>
+      paste0(
+        collapse = "\n"
+      )
+  ) |>
+  pull() |>
+  write_clip()
