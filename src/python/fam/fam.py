2bb5c012e90420e5a9bef7cd195f8d34392d071e
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-25 11:12:59 -0300

    generic families of functions

--- src/python/fam/fam.py
+++ src/python/fam/fam.py
@@ -29,6 +29,7 @@ ist = fam(
     num=lambda x: isinstance(x, Number),
 )
 
+ist()(19)
 ist.dbl(19)
 ist.dbl(19.0)
 ist.dbl("19.0")
@@ -39,53 +40,3 @@ ist.str(19)
 ist.str("19")
 ist.null(19)
 ist.null(None)
-
-
-class ist:
-    def __new__(cls, x):
-        return not ist.null(x)
-
-    def __init__():
-        pass
-
-    @staticmethod
-    def null(x):
-        return x is None
-
-    @staticmethod
-    def string(x):
-        return isinstance(x, str)
-
-    @staticmethod
-    def double(x):
-        return isinstance(x, float)
-
-    @staticmethod
-    def integer(x):
-        return isinstance(x, int)
-
-    @staticmethod
-    def numeric(x):
-        return isinstance(x, Number)
-
-
-ist("dsds")
-ist(None)
-
-ist.null("dsds")
-ist.null(None)
-
-ist.double(None)
-ist.double("dsds")
-ist.double(19)
-ist.double(19.0)
-
-ist.integer(None)
-ist.integer("dsds")
-ist.integer(19)
-ist.integer(19.0)
-
-ist.numeric(None)
-ist.numeric("dsds")
-ist.numeric(19)
-ist.numeric(19.0)
