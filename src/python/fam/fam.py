f1105023fe494436c6e0c0a3b243a8b4cf74e92f
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-25 11:21:36 -0300

    nested families of functions

--- src/python/fam/fam.py
+++ src/python/fam/fam.py
@@ -24,11 +24,29 @@ ist = fam(
     any=lambda x: x is not None,
     null=lambda x: x is None,
     str=lambda x: isinstance(x, str),
-    dbl=lambda x: isinstance(x, float),
-    int=lambda x: isinstance(x, int),
-    num=lambda x: isinstance(x, Number),
+    num=fam(
+        any=lambda x: isinstance(x, Number),
+        dbl=fam(
+            any=lambda x: isinstance(x, float),
+            pct=lambda x: all([isinstance(x, float), x >= 0, x <= 1]),
+        ),
+        int=lambda x: isinstance(x, int),
+    ),
 )
 
+ist.num.dbl.pct(19)
+ist.num.dbl.pct(1.0)
+ist.num.dbl.pct(0.0)
+ist.num.dbl.pct(-0.1)
+
+ist.num()(19)
+ist.num.dbl(19.0)
+ist.num()(19)
+ist.num()("19")
+ist.num()(19.0)
+ist.num()("19.0")
+ist.num.int(19)
+ist.num.num(19)
 ist()(19)
 ist.dbl(19)
 ist.dbl(19.0)
