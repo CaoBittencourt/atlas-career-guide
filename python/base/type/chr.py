d46e911535e16f18a803efa48dc6a2086e4c15b6
Cao Bittencourt <caobittencourt@gmail.com>
2025-06-26 20:44:32 -0300

    added url to chr type

--- python/base/type/chr.py
+++ python/base/type/chr.py
@@ -1,4 +1,5 @@
 from python.base import fam
+from re import fullmatch
 
 
 # default method
@@ -15,9 +16,28 @@ def _letter(x):
     return all([_chr(x), str(x).isalpha()])
 
 
+def _ascii(x):
+    return all([_letter(x), str(x).isascii()])
+
+
+def _url(x):
+    return (
+        True
+        if fullmatch(
+            r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})",
+            x,
+        )
+        else False
+    )
+
+
 # character string family type definitions
 chr = fam(
     chr=_chr,
+    url=_url,
     digit=_digit,
-    letter=_letter,
+    letter=fam(
+        letter=_letter,
+        ascii=_ascii,
+    ),
 )
