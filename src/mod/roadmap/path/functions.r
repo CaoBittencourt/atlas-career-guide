0663af9a3c5f965de6e67c4a4de8461217075183
CaoBittencourt <caobittencourt@gmail.com>
2025-07-22 14:01:13 -0300

    simplified dijkstra refactor

---
+++ src/mod/roadmap/path/functions.r
@@ -0,0 +1,19 @@
+#' @export
+box::use(
+  # roadmap / path / functions / base_cost[...],
+  # roadmap / path / functions / cost_util[...],
+  roadmap / path / functions / cost[...],
+  roadmap / path / functions / employment[...],
+  # roadmap / path / functions / match_vertex[...],
+  # roadmap / path / functions / path_cost[...],
+  # roadmap / path / functions / path_efficiency[...],
+  # roadmap / path / functions / path_timeline[...],
+  # roadmap / path / functions / path_util[...],
+  # roadmap / path / functions / payoff_inverse[...],
+  # roadmap / path / functions / payoff[...],
+  roadmap / path / functions / prob[...],
+  # roadmap / path / functions / prog[...],
+  roadmap / path / functions / restart[...],
+  # roadmap / path / functions / which_path[...],
+  # roadmap / path / functions / which_vertex[...],
+)
